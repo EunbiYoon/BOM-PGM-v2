19c95a2b2e8ea3dcf8926ac3fc3d310a73090a9f
Eunbi Yoon <yinyinbei0717@gmail.com>
2023-08-11 08:40:16 -0500

    werwer

--- CWebmail.py
+++ CWebmail.py
@@ -8,7 +8,7 @@ from email.mime.image import MIMEImage
 from email.mime.multipart import MIMEMultipart
 from email.mime.application import MIMEApplication 
 
-title='[테네시 재료비 관리 Task] 8월 1주차 BOM과 실제 생산 투입 재료비 차이 분석'
+title='[테네시 재료비 관리 Task] 8월 2주차 BOM과 실제 생산 투입 재료비 차이 분석'
 ################################## Send email ################################## 
 server = smtplib.SMTP('lgekrhqmh01.lge.com:25')
 server.ehlo()
@@ -27,18 +27,18 @@ msg['Bcc']='eunbi1.yoon@lge.com'
 msg['Subject']=title
 msg.attach(MIMEText('<h4 style="font-family:Arial Narrow; font-weight:500">Dear All,<br/><br/>I would like to share this week bom comparison report and detailed informations are in below website.<br/>You can access website with Chrome or Edge in CloudPC or LG wifi for security purpose. <a href="http://10.225.2.85">http://10.225.2.85</a></h4>','html'))
 
-save_path='C:/Users/RnD Workstation/Documents/NPTGERP/0804/'
+save_path='C:/Users/RnD Workstation/Documents/NPTGERP/0811/'
 
 # graph file read
-with open(save_path+'FL_0804.png', 'rb') as f:
+with open(save_path+'FL_0811.png', 'rb') as f:
         img_data = f.read()
 image3 = MIMEImage(img_data, name=os.path.basename("FL.png"))
 
-with open(save_path+'TL_0804.png', 'rb') as f:
+with open(save_path+'TL_0811.png', 'rb') as f:
         img_data = f.read()
 image1 = MIMEImage(img_data, name=os.path.basename("TL.png"))
 
-with open(save_path+'DR_0804.png', 'rb') as f:
+with open(save_path+'DR_0811.png', 'rb') as f:
         img_data = f.read()
 image2 = MIMEImage(img_data, name=os.path.basename("DR.png"))
 
@@ -51,29 +51,29 @@ msg.attach(image3)
 
 
 #첨부 파일
-etcFileName='Cost Review_0804.xlsx'
-with open("C:/Users/RnD Workstation/Documents/NPTGERP/0804/result_0804.xlsx", 'rb') as etcFD : 
+etcFileName='Cost Review_0811.xlsx'
+with open("C:/Users/RnD Workstation/Documents/NPTGERP/0811/result_0811.xlsx", 'rb') as etcFD : 
     etcPart = MIMEApplication( etcFD.read() )
     #첨부파일의 정보를 헤더로 추가
     etcPart.add_header('Content-Disposition','attachment', filename=etcFileName)
     msg.attach(etcPart)
 
-etcFileName='BOM Comparison_FL_0804.xlsx'
-with open("C:/Users/RnD Workstation/Documents/NPTGERP/0804/BOM Comparison_FL.xlsx", 'rb') as etcFD : 
+etcFileName='BOM Comparison_FL_0811.xlsx'
+with open("C:/Users/RnD Workstation/Documents/NPTGERP/0811/BOM Comparison_FL.xlsx", 'rb') as etcFD : 
     etcPart = MIMEApplication( etcFD.read() )
     #첨부파일의 정보를 헤더로 추가
     etcPart.add_header('Content-Disposition','attachment', filename=etcFileName)
     msg.attach(etcPart)
 
-etcFileName='BOM Comparison_TL_0804.xlsx'
-with open("C:/Users/RnD Workstation/Documents/NPTGERP/0804/BOM Comparison_TL.xlsx", 'rb') as etcFD : 
+etcFileName='BOM Comparison_TL_0811.xlsx'
+with open("C:/Users/RnD Workstation/Documents/NPTGERP/0811/BOM Comparison_TL.xlsx", 'rb') as etcFD : 
     etcPart = MIMEApplication( etcFD.read() )
     #첨부파일의 정보를 헤더로 추가
     etcPart.add_header('Content-Disposition','attachment', filename=etcFileName)
     msg.attach(etcPart)
 
-etcFileName='BOM Comparison_DR_0804.xlsx'
-with open("C:/Users/RnD Workstation/Documents/NPTGERP/0804/BOM Comparison_DR.xlsx", 'rb') as etcFD : 
+etcFileName='BOM Comparison_DR_0811.xlsx'
+with open("C:/Users/RnD Workstation/Documents/NPTGERP/0811/BOM Comparison_DR.xlsx", 'rb') as etcFD : 
     etcPart = MIMEApplication( etcFD.read() )
     #첨부파일의 정보를 헤더로 추가
     etcPart.add_header('Content-Disposition','attachment', filename=etcFileName)
